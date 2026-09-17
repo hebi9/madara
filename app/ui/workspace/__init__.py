2e189f1f1dafb6a02ee66da8fdd787424946eaef
Hebi <99999432+hebi9@users.noreply.github.com>
2026-09-16 21:30:39 -0600

    refactor(workspace): add workspace support components

---
+++ app/ui/workspace/__init__.py
@@ -0,0 +1,9 @@
+from .selection_manager import SelectionManager
+from .workspace_renderer import WorkspaceRenderer
+from .workspace_scene import WorkspaceScene
+
+__all__ = [
+    "SelectionManager",
+    "WorkspaceRenderer",
+    "WorkspaceScene",
+]
